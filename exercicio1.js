317c550d323f133bd956cc38b65c258f35940311
augustosd3 <augustosd3@gmail.com>
2017-04-13 11:32:54 -0300

    GO

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,20 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+let buttonExpand = document.querySelectorAll('.botao-expandir-retrair');
+
+for(let exp of buttonExpand){
+	exp.addEventListener('click', function(evt){
+			let par = evt.target.parentNode;
+			if(par.classList.contains('expandido')){
+				par.classList.remove('expandido');
+				evt.target.innerHTML = '+';
+			}
+			else{
+				par.classList.add('expandido');
+				evt.target.innerHTML= '-';
+			}
+		
+	});
+}
+
